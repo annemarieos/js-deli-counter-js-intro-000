b239403bde21e36ae893e172c82c0ad98ad4fc13
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-03-16 17:48:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(katzDeliLine, name) {
 
 function nowServing (katzDeliLine) {
   if (katzDeliLine > []) {
-    return `Currently serving ${katzDeliLine.shift()} ${name}.`
+    return `Currently serving ${katzDeliLine.shift()}.`
   } else {
     return `There is nobody waiting to be served!`
   }
